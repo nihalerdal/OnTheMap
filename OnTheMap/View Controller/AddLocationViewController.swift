37a881e08c3346a955aaa49d765f272160fcf9dd
Nihal Erdal <nihalerdall@gmail.com>
2021-06-10 20:04:30 -0700

    getting coordinate from address

--- OnTheMap/View Controller/AddLocationViewController.swift
+++ OnTheMap/View Controller/AddLocationViewController.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import MapKit
 
 class AddLocationViewController: UIViewController {
 
     @IBOutlet weak var locationTextField: UITextField!
     @IBOutlet weak var linkTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,6 +26,37 @@ class AddLocationViewController: UIViewController {
     
     @IBAction func cancel(_ sender: Any) {
     }
+    
+    
+    func findGeocode(_ address: String){
+        CLGeocoder().geocodeAddressString(address) { (placemark, error)
+            in
+            if error == nil {
+                
+                if let placemark = placemark?.first,
+                   let location = placemark.location {
+                    let latitude = location.coordinate.latitude
+                    let longitude = location.coordinate.longitude
+                    
+                    print("Latitude:\(latitude), Longitude:\(longitude)")
+                    
+                }
+            }else {
+                fatalError("geocode error")
+            }
+        }
+        
+    }
+
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "FindLocationSegue"{
+//            if let mapVC = segue.destination as? FindLocationViewController{
+//                let
+//            }
+//
+//        }
+//    }
     /*
     // MARK: - Navigation
 
