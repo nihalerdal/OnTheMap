bcfa319c641d5e6cf17c6274b981e11d9c150d01
Nihal Erdal <61894386+nihalerdal@users.noreply.github.com>
2021-07-01 11:15:38 -0700

    Add files via upload

---
+++ StudentModel.swift
@@ -0,0 +1,16 @@
+//
+//  StudentModel.swift
+//  OnTheMap
+//
+//  Created by Nihal Erdal on 6/30/21.
+//
+
+import Foundation
+
+struct Student {
+    static var locations = [StudentLocation]()
+    static var link : String = ""
+    static var location : String = ""
+    static var latitude : Double = 0.0
+    static var longitude: Double = 0.0
+}
